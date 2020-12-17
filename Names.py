1490c9449588452c8464094d605d97794369b3e7
Chief-kith <63549784+Chief-kith@users.noreply.github.com>
2020-12-17 19:54:32 +0300

    Add files via upload

---
+++ Names.py
@@ -0,0 +1,4 @@
+Word=["abroad","beauty","centre","decade","eighty","finish","flying",
+        "gather","hidden","impact","joseph","labour","manage","nearly",
+        "office","palace","patent","record","sample","tissue","united",
+        "volume","weekly","yellow"]
